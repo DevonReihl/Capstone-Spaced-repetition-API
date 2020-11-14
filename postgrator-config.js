f37e358632cac99157540d7e3b443c889c46bfb0
Devon Reihl <devon.reihl@gmail.com>
2020-11-14 14:40:33 -0800

    update DB to DATABASE

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
-  "host": process.env.MIGRATION_DB_HOST,
-  "port": process.env.MIGRATION_DB_PORT,
-  "database": process.env.MIGRATION_DB_NAME,
-  "username": process.env.MIGRATION_DB_USER,
-  "password": process.env.MIGRATION_DB_PASS
+  "host": process.env.MIGRATION_DATABASE_HOST,
+  "port": process.env.MIGRATION_DATABASE_PORT,
+  "database": process.env.MIGRATION_DATABASE_NAME,
+  "username": process.env.MIGRATION_DATABASE_USER,
+  "password": process.env.MIGRATION_DATABASE_PASS
 }
